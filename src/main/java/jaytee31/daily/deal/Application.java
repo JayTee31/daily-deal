5d619112a87b4416c7a07d6ae36757cb5ca6b092
Tamas Jarvas <jtamas31@gmail.com>
2021-12-19 20:14:50 +0100

    base structure 2.0

--- src/main/java/jaytee31/daily/deal/Application.java
+++ src/main/java/jaytee31/daily/deal/Application.java
@@ -1,7 +1,7 @@
-package daily.deal;
+package jaytee31.daily.deal;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hallo");
+        System.out.println("hello");
     }
 }
